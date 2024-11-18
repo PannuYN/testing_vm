1aef52f49c13e76dff3ffc1b3312e054b9a83d6f
Pannu YN <pannuyannaing.fw@gmail.com>
2024-11-18 12:51:34 +0630

    updated

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can change the port if needed
+const port = 3000;
 
-// Simple route to test
 app.get('/', (req, res) => {
     res.send('Hello, your VM public IP address works!');
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${port}`);
 });
